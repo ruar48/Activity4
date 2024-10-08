1caa9782fdb0cc3e5b0879181b396790349fe1ad
ruar48 <133135612+ruar48@users.noreply.github.com>
2024-10-08 09:44:39 +0800

    a

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,13 +32,13 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('/'),
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('/'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
